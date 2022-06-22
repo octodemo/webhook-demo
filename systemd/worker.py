3fc90a74da8a18b68b26d42e67990381db765827
Natalie Somersall <some-natalie@github.com>
2022-06-22 08:45:13 -0600

    blacken all the things

--- systemd/worker.py
+++ systemd/worker.py
@@ -3,13 +3,13 @@
 import redis
 from rq import Worker, Queue, Connection
 
-listen = ['default']
+listen = ["default"]
 
-redis_url = 'redis://localhost:16379'
+redis_url = "redis://localhost:16379"
 
 conn = redis.from_url(redis_url)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with Connection(conn):
         worker = Worker(list(map(Queue, listen)))
         worker.work()
